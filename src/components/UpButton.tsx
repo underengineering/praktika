7e3826ecef850f2a241dd09ef86f5a00a81127d0
underengineering <san4a852b@gmail.com>
2023-11-01 21:04:53 +0300

    feat: Implement Upbutton functionality

--- src/components/UpButton.tsx
+++ src/components/UpButton.tsx
@@ -1,16 +1,12 @@
-import { FC } from "react";
+"use client";
 
 import { Straight } from "@mui/icons-material";
 
-interface Props {
-    onClick?: () => void;
-}
-
-const UpButton: FC<Props> = ({ onClick }) => {
+const UpButton = () => {
     return (
         <button
             className="flex h-[72px] w-[72px] items-center justify-center rounded-full bg-primary shadow-primary transition-[width,height,box-shadow] hover:h-[82px] hover:w-[82px] hover:shadow-2xl active:h-[82px] active:w-[82px] active:shadow-2xl"
-            onClick={onClick}
+            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
         >
             <Straight className="fill-stone-50" sx={{ fontSize: 36 }} />
         </button>
