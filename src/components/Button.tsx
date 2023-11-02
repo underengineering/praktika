5de87df60e2a95fd2bddfbdd9c8936c4450ebcff
underengineering <san4a852b@gmail.com>
2023-11-02 13:50:19 +0300

    feat: Forward props in Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,5 @@
 import { Roboto } from "next/font/google";
-import { FC } from "react";
+import { ButtonHTMLAttributes, FC } from "react";
 
 const roboto = Roboto({
     weight: "400",
@@ -7,17 +7,17 @@ const roboto = Roboto({
     variable: "--font-sans",
 });
 
-interface Props {
-    className?: string;
-    children: string;
-}
-
-const Button: FC<Props> = ({ children, className }) => {
+const Button: FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
+    children,
+    className,
+    ...props
+}) => {
     return (
         <button
             className={`bg-primary px-[25px] py-[14px] text-sm text-stone-50 transition-[padding] hover:px-[25px] hover:py-[16px] active:px-[25px] active:py-[16px] ${
                 roboto.variable
             } font-sans ${className !== undefined ? className : ""}`}
+            {...props}
         >
             {children}
         </button>
