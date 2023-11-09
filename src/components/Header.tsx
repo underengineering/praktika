6230098802fc3998b193e90e84663daf23a2058c
underengineering <san4a852b@gmail.com>
2023-11-09 11:08:04 +0300

    feat: Search in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { Roboto } from "next/font/google";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 import SideBar from "@/components/SideBar";
+import { buildSearchParams } from "@/lib/api";
 import {
     FavoriteBorderOutlined,
     Menu,
@@ -21,8 +23,13 @@ const roboto = Roboto({
 });
 
 const Header = () => {
+    const router = useRouter();
+
+    const [query, setQuery] = useState("");
+
     const [sideBarOpened, setSideBarOpened] = useState(false);
     const [userMenuOpened, setUserMenuOpened] = useState(false);
+
     return (
         <header
             className={`flex w-full flex-col justify-between px-2 py-3 sm:px-36 sm:py-12 ${
@@ -37,14 +44,24 @@ const Header = () => {
                     <Menu sx={{ fontSize: 36 }} />
                 </button>
                 <div className="flex grow items-center gap-9 sm:grow-0">
-                    <div className="flex grow gap-4 rounded-xl bg-stone-50 p-1 sm:bg-transparent sm:p-0">
+                    <form
+                        className="flex grow gap-4 rounded-xl bg-stone-50 p-1 sm:bg-transparent sm:p-0"
+                        onSubmit={(ev) => {
+                            ev.preventDefault();
+                            router.replace(
+                                `/search?${buildSearchParams({ name: query })}`
+                            );
+                        }}
+                    >
                         <SearchOutlined />
                         <input
                             className="w-full border-b-0 border-b-zinc-500 bg-transparent sm:w-[308px] sm:border-b"
                             placeholder="Поиск"
                             type="text"
+                            value={query}
+                            onChange={(ev) => setQuery(ev.currentTarget.value)}
                         />
-                    </div>
+                    </form>
                     <div className="relative hidden gap-3 sm:flex">
                         <button
                             onClick={() => setUserMenuOpened(!userMenuOpened)}
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -15,6 +15,7 @@ export interface IProduct {
     discountedPrice?: number;
     rating: number;
     favorited: boolean;
+    tags: string[];
 }
 
 export interface IReview<TStatus = TDeserialized> {
@@ -24,6 +25,11 @@ export interface IReview<TStatus = TDeserialized> {
     createdAt: DateSerializeable<TStatus>;
 }
 
+export interface ITag {
+    name: string;
+    displayName: string;
+}
+
 export interface IUser {
     fullName: string;
     phone: string;
@@ -31,10 +37,14 @@ export interface IUser {
 }
 
 export interface ISearchQuery {
-    type?: string;
+    name?: string;
+    tags?: string[];
     sex?: "male" | "female";
+    sort?: "popularity" | "rating" | "price" | "discount" | "updates";
     ageGt?: number;
     ageLt?: number;
+    priceGt?: number;
+    priceLt?: number;
     size?: string;
     hasDiscount?: boolean;
     favorited?: boolean;
@@ -42,10 +52,16 @@ export interface ISearchQuery {
 
 export function buildSearchParams(query: ISearchQuery) {
     const url = new URLSearchParams();
-    if (query.type !== undefined) url.set("type", query.type);
+    if (query.name !== undefined) url.set("name", query.name);
+    if (query.tags !== undefined) url.set("tags", query.tags.join(","));
     if (query.sex !== undefined) url.set("sex", query.sex);
+    if (query.sort !== undefined) url.set("sort", query.sort);
     if (query.ageGt !== undefined) url.set("ageGt", query.ageGt.toString());
     if (query.ageLt !== undefined) url.set("ageLt", query.ageLt.toString());
+    if (query.priceGt !== undefined)
+        url.set("priceGt", query.priceGt.toString());
+    if (query.priceLt !== undefined)
+        url.set("priceLt", query.priceLt.toString());
     if (query.size !== undefined) url.set("size", query.size);
     if (query.hasDiscount !== undefined)
         url.set("hasDiscount", query.hasDiscount.toString());
@@ -58,18 +74,30 @@ export function buildSearchParams(query: ISearchQuery) {
 export function parseSearchQuery(params: URLSearchParams): ISearchQuery {
     const searchQuery: ISearchQuery = {};
 
-    const type = params.get("type");
-    if (type !== null) searchQuery.type = type;
+    const name = params.get("name");
+    if (name !== null) searchQuery.name = name;
+
+    const tags = params.get("tags");
+    if (tags !== null) searchQuery.tags = tags.split(",");
 
     const sex = params.get("sex");
     if (sex !== null && (sex === "male" || sex === "female"))
         searchQuery.sex = sex;
+
+    const sort = params.get("sort");
+    if (sort !== null) searchQuery.sort = sort as any;
 
     const ageGt = params.get("ageGt");
     if (ageGt !== null) searchQuery.ageGt = +ageGt;
 
     const ageLt = params.get("ageLt");
     if (ageLt !== null) searchQuery.ageLt = +ageLt;
+
+    const priceGt = params.get("priceGt");
+    if (priceGt !== null) searchQuery.priceGt = +priceGt;
+
+    const priceLt = params.get("priceLt");
+    if (priceLt !== null) searchQuery.priceLt = +priceLt;
 
     const size = params.get("size");
     if (size !== null) searchQuery.size = size;
@@ -87,16 +115,18 @@ export interface IDatabase<TStatus = TDeserialized> {
     products: IProduct[];
     reviews: IReview<TStatus>[];
     users: IUser[];
+    tags: ITag[];
 }
 
 const database: IDatabase = {
     products: [
         {
             name: "Блузка женская классная",
-            price: 65,
-            discountedPrice: 50,
+            price: 0,
+            discountedPrice: 800,
             rating: 4,
             favorited: false,
+            tags: ["skirt"],
         },
         {
             name: "Блузка мужская базированная",
@@ -104,6 +134,14 @@ const database: IDatabase = {
             discountedPrice: 50,
             rating: 4,
             favorited: false,
+            tags: ["skirt"],
+        },
+        {
+            name: "штоны)",
+            price: 90,
+            rating: 1,
+            favorited: false,
+            tags: ["shorts"],
         },
         {
             name: "Блузка моя базированная",
@@ -111,6 +149,7 @@ const database: IDatabase = {
             discountedPrice: 50,
             rating: 4,
             favorited: false,
+            tags: ["skirt"],
         },
         {
             name: "Блузка моя базированная",
@@ -118,6 +157,7 @@ const database: IDatabase = {
             discountedPrice: 50,
             rating: 4,
             favorited: false,
+            tags: ["skirt"],
         },
         {
             name: "Блузка моя базированная",
@@ -125,13 +165,7 @@ const database: IDatabase = {
             discountedPrice: 50,
             rating: 4,
             favorited: false,
-        },
-        {
-            name: "Блузка моя базированная",
-            price: 65,
-            discountedPrice: 50,
-            rating: 4,
-            favorited: false,
+            tags: ["skirt"],
         },
     ],
     reviews: [
@@ -149,6 +183,19 @@ const database: IDatabase = {
         },
     ],
     users: [],
+    tags: [
+        { name: "tshirt", displayName: "Майки" },
+        { name: "costume", displayName: "Костюмы" },
+        { name: "trousers", displayName: "Брюки" },
+        { name: "jeans", displayName: "Джинсы" },
+        { name: "skirt", displayName: "Юбки" },
+        { name: "shorts", displayName: "Шорты" },
+        { name: "hoody", displayName: "Свитшоты, худи" },
+        { name: "blouses", displayName: "Блузки и рубашки" },
+        { name: "jacket", displayName: "Пиджаки и жакеты" },
+        { name: "dress", displayName: "Платья и сарафаны" },
+        { name: "outerwear", displayName: "Верхняя одежда" },
+    ],
 };
 
 function serialize(database: IDatabase<TDeserialized>): IDatabase<TSerialized> {
